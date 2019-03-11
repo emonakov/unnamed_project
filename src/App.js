464c73a5f79a2ef898e777376a39f2d2049d7dad
Eugene Monakoff <montemonakov@gmail.com>
2019-03-11 20:25:54 +0000

    +1 init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import './assets/_colors.scss';
+import './assets/_styles.scss';
 
 class App extends Component {
   render() {
